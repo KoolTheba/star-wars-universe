09c94437ae94ea0b53620c03b39ded638eb1310d
KoolTheba <gomez.teba@gmail.com>
2022-09-03 19:06:30 +0200

    feat: added search values to the list

--- components/FilmList.js
+++ components/FilmList.js
@@ -5,10 +5,15 @@ import FilmCard from './FilmCard'
 import styles from '../styles/FilmList.module.css'
 
 const FilmList = ({filmsList, searchContext}) => {
+    const searchContextElements = searchContext.split(' ').map((el) => el)
+
     return (
         <>
             <div className={styles.cardsWrapper}>
-                <h3 className={styles.title}>Your films list!</h3>
+                <div className={styles.titleWrapper}>
+                    <h3 className={styles.title}>Your films list for your search:</h3>
+                    {searchContextElements.map((el, index) => <span key={index} className={styles.context}>{el}</span>)}
+                </div>
                 <ul className={styles.cardsList}>
                 {filmsList.map((film) => (
                     <li key={film.episode_id} className={styles.cardItem}>
