c40e8405efc730f54cede2481e437f157627f099
KoolTheba <gomez.teba@gmail.com>
2022-09-03 09:22:50 +0200

    feat: added years passed to film cards

--- components/FilmCard.js
+++ components/FilmCard.js
@@ -3,11 +3,13 @@ import React from "react"
 import styles from '../styles/FilmCard.module.css'
 
 const FilmCard = ({film}) => {
+    const yearsPassed =  new Date(new Date() - new Date(film.release_date)).getFullYear() - 1970
+
     return (
         <>
             <p className={styles.cardInfo}>{film.title}</p>
             <p className={styles.cardInfo}>{`Episode ${film.episode_id}`}</p>
-            <p className={styles.cardInfo}>{film.release_date}</p>
+            <p className={styles.cardInfo}>{film.release_date} {`(${yearsPassed} years ago)`}</p>
         </>
     )
 }
