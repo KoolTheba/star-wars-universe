c2e687792e766cba2b08098ad2a10a41d8383ae8
KoolTheba <gomez.teba@gmail.com>
2022-09-03 17:16:17 +0200

    feat: passed search context to Film Card to be reused after

--- components/FilmCard.js
+++ components/FilmCard.js
@@ -1,15 +1,20 @@
 import React from "react"
+import Link from 'next/link'
 
 import styles from '../styles/FilmCard.module.css'
 
-const FilmCard = ({film}) => {
+const FilmCard = ({film, searchContext}) => {
     const yearsPassed =  new Date(new Date() - new Date(film.release_date)).getFullYear() - 1970
 
     return (
         <>
-            <p className={styles.cardInfo}>{film.title}</p>
-            <p className={styles.cardInfo}>{`Episode ${film.episode_id}`}</p>
-            <p className={styles.cardInfo}>{film.release_date} {`(${yearsPassed} years ago)`}</p>
+            <Link href={`/film/${encodeURIComponent(film.episode_id)}/?search=${searchContext}`}>
+                <a>
+                    <p className={styles.cardInfo}>{film.title}</p>
+                    <p className={styles.cardInfo}>{`Episode ${film.episode_id}`}</p>
+                    <p className={styles.cardInfo}>{film.release_date} {`(${yearsPassed} years ago)`}</p>
+                </a>
+          </Link>
         </>
     )
 }
