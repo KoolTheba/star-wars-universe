67e8895de43f16edb8a44ccd79a7934cf12c92de
KoolTheba <gomez.teba@gmail.com>
2022-12-06 17:34:05 +0100

    test: set up all handlers and added mock for people detail

--- mocks/handlers/index.js
+++ mocks/handlers/index.js
@@ -1,12 +1,50 @@
-import { rest } from "msw";
+import { rest } from 'msw'
 
-import data from '../data/filmDetail.json'
+import filmDetail from '../data/filmDetail.json'
+import peopleDetail from '../data/peopleDetail.json'
+
+const emptyResults = {
+  count: 0,
+  next: null,
+  previous: null,
+  results: []
+}
 
 export const handlers = [
-    rest.get("https://swapi.dev/api/films/4", (req, res, ctx) => {
-        return res(
-          ctx.status(200),
-          ctx.json(data)
-        );
-    })
-];
+  rest.get('https://swapi.dev/api/people?search=luke', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json(peopleDetail)
+    )
+  }),
+  rest.get('https://swapi.dev/api/films?search=luke', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json(emptyResults)
+    )
+  }),
+  rest.get('https://swapi.dev/api/planets?search=luke', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json(emptyResults)
+    )
+  }),
+  rest.get('https://swapi.dev/api/starships?search=luke', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json(emptyResults)
+    )
+  }),
+  rest.get('https://swapi.dev/api/vehicles?search=luke', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json(emptyResults)
+    )
+  }),
+  rest.get('https://swapi.dev/api/films/4', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json(filmDetail)
+    )
+  })
+]
