7dc8cc0f0b9689bf736550fd4d2c81da2e083319
KoolTheba <gomez.teba@gmail.com>
2022-12-06 20:32:18 +0100

    test: removed mocks from coverage report

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,19 @@
 const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
-  dir: './',
+  dir: './'
 })
 
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '^@/components/(.*)$': '<rootDir>/components/$1',
-    '^@/pages/(.*)$': '<rootDir>/pages/$1',
+    '^@/pages/(.*)$': '<rootDir>/pages/$1'
   },
   testEnvironment: 'jsdom',
+  collectCoverageFrom: [
+    'src/**/{!(ignore-me),}.js'
+  ]
 }
 
 module.exports = createJestConfig(customJestConfig)
